ab1f0d76b82da0a24f3871675042fd0676a30218
nie-sir <2807954557@qq.com>
2015-04-09 22:51:13 +0800

    通风1聂朝刚的作业1403050114

---
+++ test1-2.py
@@ -0,0 +1,23 @@
+# -*- encoding:utf-8 -*-
+
+#姓名：聂朝刚
+#学号：1403050114
+#班级：通风14-1
+#e(3)
+import math
+f1=(math.exp(3*0.1)-math.exp(3))/0.1
+print f1
+f2=(math.exp(3*0.01)-math.exp(3))/0.01
+print f2
+f3=(math.exp(3*0.001)-math.exp(3))/0.001
+print f3
+f4=(math.exp(3*0.0001)-math.exp(3))/0.0001
+print f4
+f5=(math.exp(3*0.00001)-math.exp(3))/0.00001
+print f5
+g5=math.exp(3)
+print g5
+
+# -*- encoding:utf-8 -*-
+print '近似导数：',f5
+print '精确导数值：',g5
